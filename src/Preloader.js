62ca737aa46a95109507d707749eae13ef28bc11
tertiux44 <secondsonofadebayo@gmail.com>
2023-02-05 23:12:54 +0100

    fixing ui

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 export default function Preloader(props){
+    const [identity,setIdentity] = React.useState(true)
+
+    React.useEffect(
+        function(){
+            setTimeout(
+                function(){
+                    setIdentity(false)
+                }
+            )
+        }
+    )
+
+
     return (
-        <div className={props.className}>
+        <div className={identity?"priority preloader type-big":props.className}>
             <div className="preloader-circle"></div>
             {/* <div className="getting-data"></div> */}
         </div>
