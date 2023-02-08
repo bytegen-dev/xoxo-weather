12df86f7640ab770b831a4f78a87530986e1dfba
tertiux44 <secondsonofadebayo@gmail.com>
2023-02-08 08:26:54 +0100

    preparing UI for API integration

--- src/App.js
+++ src/App.js
@@ -286,75 +286,75 @@ export default function App(){
     React.useEffect(
         function(){
 
-            async function getData(){
-                const dateInfo =  await new Date()
-                let day = dateInfo.getDay()
-                const date = dateInfo.getDate()
-                let month = dateInfo.getMonth()
-                const year = dateInfo.getFullYear()
+            // async function getData(){
+            // }
+            const dateInfo =  new Date()
+            let day = dateInfo.getDay()
+            const date = dateInfo.getDate()
+            let month = dateInfo.getMonth()
+            const year = dateInfo.getFullYear()
 
-                if(day === 0){
-                    day = "Sunday"
-                } else if(day === 1){
-                    day = "Monday"
-                } else if(day === 2){
-                    day = "Tuesday"
-                } else if(day === 3){
-                    day = "Wednesday"
-                } else if(day === 4){
-                    day = "Thursday"
-                } else if(day === 5){
-                    day = "Friday"
-                } else if(day === 6){
-                    day = "Saturday"
-                }
-
-                if(month === 0){
-                    month = "January"
-                } else if(month === 1){
-                    month = "February"
-                } else if(month === 2){
-                    month = "March"
-                } else if(month === 3){
-                    month = "April"
-                } else if(month === 4){
-                    month = "May"
-                } else if(month === 5){
-                    month = "June"
-                } else if(month === 6){
-                    month = "July"
-                } else if(month === 7){
-                    month = "August"
-                } else if(month === 8){
-                    month = "September"
-                } else if(month === 9){
-                    month = "October"
-                } else if(month === 10){
-                    month = "November"
-                } else if(month === 11){
-                    month = "December"
-                }
-
-                console.log(
-                    day, date, month, year
-                )
-
-                setTodayDate(
-                    function(prevState){
-                        return(
-                            {
-                                ...prevState,
-                                day: day,
-                                date: date,
-                                month: month,
-                                year: year
-                            }
-                        )
-                    }
-                )
+            if(day === 0){
+                day = "Sunday"
+            } else if(day === 1){
+                day = "Monday"
+            } else if(day === 2){
+                day = "Tuesday"
+            } else if(day === 3){
+                day = "Wednesday"
+            } else if(day === 4){
+                day = "Thursday"
+            } else if(day === 5){
+                day = "Friday"
+            } else if(day === 6){
+                day = "Saturday"
             }
 
-            window.addEventListener("load", getData)
+            if(month === 0){
+                month = "January"
+            } else if(month === 1){
+                month = "February"
+            } else if(month === 2){
+                month = "March"
+            } else if(month === 3){
+                month = "April"
+            } else if(month === 4){
+                month = "May"
+            } else if(month === 5){
+                month = "June"
+            } else if(month === 6){
+                month = "July"
+            } else if(month === 7){
+                month = "August"
+            } else if(month === 8){
+                month = "September"
+            } else if(month === 9){
+                month = "October"
+            } else if(month === 10){
+                month = "November"
+            } else if(month === 11){
+                month = "December"
+            }
+
+            console.log(
+                day, date, month, year
+            )
+
+            setTodayDate(
+                function(prevState){
+                    return(
+                        {
+                            ...prevState,
+                            day: day,
+                            date: date,
+                            month: month,
+                            year: year
+                        }
+                    )
+                }
+            )
+
+            // window.addEventListener("load", getData)
 
         }, []
     )
